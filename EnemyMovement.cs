a36e2bfff727d34a6e2380234056093fd3d616f5
Farbod <farbodsaberi@gmail.com>
2024-11-03 21:18:48 +0330

    Add files via upload

---
+++ EnemyMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour 
+{
+
+	public float Speed = 2f;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.position += Vector3.left * Speed * Time.deltaTime;
+	}
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour 
+{
+
+	public float Speed = 10f;
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		Vector3 movement = new Vector3(moveHorizontal, moveVertical, 0);
+		transform.position += movement * Speed * Time.deltaTime;
+
+
+	}
+
+
+
+
+}
+
